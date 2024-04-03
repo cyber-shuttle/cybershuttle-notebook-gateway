ed113e04267813e5d241dbf66ead6e72c728b098
Yasith Jayawardana <mail@yasith.dev>
2024-04-03 17:37:04 -0400

    add username arg to kernel sync daemon

--- cybershuttle_nbplugin/kernel_sync_daemon.py
+++ cybershuttle_nbplugin/kernel_sync_daemon.py
@@ -17,7 +17,7 @@ import schedule
 
 def sync_cybershuttle_kernels_with_localfs():
     try:
-        response = requests.get(f"{url}/kernelspecs")
+        response = requests.get(f"{url}/kernelspecs", params={"user": username})
         if response.status_code == 200:
             data: dict[str, dict] = response.json()
             for kernel_name, kernel_spec in data.items():
@@ -34,16 +34,17 @@ def sync_cybershuttle_kernels_with_localfs():
 
 
 if __name__ == "__main__":
-    # Schedule the job to run every hour
-    N = 5
     parser = argparse.ArgumentParser(description="Periodically sync available kernels with Cybershuttle Gateway")
     parser.add_argument("--url", "-u", type=str, help="URL of Cybershuttle Gateway Server")
-    parser.add_argument("--kernel_dir", "-k,", type=str, help="Path to store kernel specs")
+    parser.add_argument("--username", "-U", type=str, help="Username for Cybershuttle Gateway Server")
+    parser.add_argument("--kernel_dir", "-k,", type=str, help="Path to store kernel specs locally")
     args = parser.parse_args()
     url = args.url
-    kernel_dir = os.path.expandvars(args.kernel_dir)
-    kernel_dir = Path(kernel_dir).expanduser().absolute()
+    username = args.username
+    kernel_dir = Path(os.path.expandvars(args.kernel_dir)).expanduser().absolute()
 
+    # run every 5 seconds
+    N = 5
     schedule.every(N).seconds.do(sync_cybershuttle_kernels_with_localfs)
 
     while True:
