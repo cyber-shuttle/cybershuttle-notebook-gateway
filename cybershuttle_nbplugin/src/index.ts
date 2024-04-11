dd063d673dbdc78b4f70b8386390211f4563cede
Yasith Jayawardana <mail@yasith.dev>
2024-04-11 13:44:54 -0400

    fix issues in notebook plugin

--- cybershuttle_nbplugin/src/index.ts
+++ cybershuttle_nbplugin/src/index.ts
@@ -2,24 +2,29 @@ import {
   JupyterFrontEnd,
   JupyterFrontEndPlugin
 } from '@jupyterlab/application';
+import { DocumentRegistry, IDocumentWidget } from '@jupyterlab/docregistry';
 import { ILauncher } from '@jupyterlab/launcher';
+import { INotebookModel, NotebookPanel } from '@jupyterlab/notebook';
 import { ISettingRegistry } from '@jupyterlab/settingregistry';
 
+import { ToolbarButton } from '@jupyterlab/ui-components';
+
+import { Dialog } from '@jupyterlab/apputils';
+import { IDisposable } from '@lumino/disposable';
 import { CybershuttleKernelLauncher } from './kernel_launcher';
-import { showDialog, Dialog } from '@jupyterlab/apputils';
 
 /**
  * Initialization data for the cybershuttle_nbplugin extension.
  */
 const plugin: JupyterFrontEndPlugin<void> = {
-  id: 'cybershuttle_nbplugin:plugin',
+  id: `cybershuttle_nbplugin:plugin`,
   description: 'Notebook Extension for Running Remote Kernels via Cybershuttle',
   autoStart: true,
   optional: [ISettingRegistry, ILauncher],
   activate: activate_cybershuttle_launcher
 };
 
-function activate_cybershuttle_launcher(
+async function activate_cybershuttle_launcher(
   app: JupyterFrontEnd,
   settingRegistry: ISettingRegistry,
   launcher: ILauncher
@@ -27,77 +32,136 @@ function activate_cybershuttle_launcher(
   console.log('JupyterLab extension cybershuttle_nbplugin is activated!');
 
   if (settingRegistry) {
-    settingRegistry
-      .load(plugin.id)
-      .then(settings => {
-        console.log(
-          'cybershuttle_nbplugin settings loaded:',
-          settings.composite
-        );
-      })
-      .catch(reason => {
-        console.error(
-          'Failed to load settings for cybershuttle_nbplugin.',
-          reason
-        );
-      });
+    try {
+      const settings = await settingRegistry.load(plugin.id);
+      console.log('cybershuttle_nbplugin settings loaded:', settings.composite);
+    } catch (reason) {
+      console.error('error loading cybershuttle_nbplugin settings:', reason);
+    }
   }
 
-  const CMD_LAUNCH = 'cybershuttle:launch_kernel';
+  const CMD_CREATE = 'cybershuttle:create_kernel';
+  const CMD_UPDATE = 'cybershuttle:switch_kernel';
   const CMD_CHOOSE = 'cybershuttle:pick_kernel';
+  type X = DocumentRegistry.IWidgetExtension<NotebookPanel, INotebookModel>;
 
-  app.commands.addCommand(CMD_LAUNCH, {
-    label: 'Launch Cybershuttle Kernel',
-    execute: async specs => {
-      // launch kernel and open notebook
-      console.log(specs);
+  class CybershuttleButton implements X {
+    createNew(
+      panel: NotebookPanel,
+      context: DocumentRegistry.IContext<INotebookModel>
+    ): IDisposable {
+      // Create the toolbar button
+      let cybershuttle = new ToolbarButton({
+        label: 'Cybershuttle Kernel',
+        onClick: () => app.commands.execute(CMD_CHOOSE, { action: CMD_UPDATE })
+      });
+
+      // Add the toolbar button to the notebook toolbar
+      panel.toolbar.insertItem(10, 'cybershuttle', cybershuttle);
+
+      // The ToolbarButton class implements `IDisposable`, so the
+      // button *is* the extension for the purposes of this method.
+      return cybershuttle;
+    }
+  }
+
+  app.commands.addCommand(CMD_UPDATE, {
+    label: 'Change to Cybershuttle Kernel',
+    execute: async args => {
+      try {
+        // save args to kernelspec
+        console.log(args);
+        // reload kernelspecs
+        const services = app.serviceManager;
+        await services.ready;
+        const specs = services.kernelspecs;
+        await specs.ready;
+        // get kernel info
+        const kernel_name = 'gkeyll';
+        // update notebook with new kernel
+        const cw = app.shell.currentWidget;
+        if (cw! instanceof NotebookPanel) {
+          const notebookPanel = cw;
+          const sessionContext = notebookPanel.sessionContext;
+          await sessionContext.changeKernel({});
+          return sessionContext.changeKernel({ name: kernel_name });
+        }
+      } catch (err) {
+        console.error(err);
+      }
+    }
+  });
+
+  app.commands.addCommand(CMD_CREATE, {
+    label: 'Create With Cybershuttle Kernel',
+    execute: async args => {
+      try {
+        // save args to kernelspec
+        console.log(args);
+        // reload kernelspecs
+        const services = app.serviceManager;
+        await services.ready;
+        const specs = services.kernelspecs;
+        await specs.ready;
+        // launch kernel and open notebook
+        const cwd = '.';
+        const kernel_name = 'gkeyll';
+        // open notebook with kernel
+        const widget = (await app.commands.execute('docmanager:open', {
+          path: `${cwd}/example.ipynb`,
+          factory: 'Notebook',
+          kernel: {}
+        })) as IDocumentWidget;
+        widget.isUntitled = true;
+        return widget.context.sessionContext.changeKernel({
+          name: kernel_name
+        });
+      } catch (err) {
+        console.error(err);
+      }
     }
   });
 
   app.commands.addCommand(CMD_CHOOSE, {
-    label: 'Pick Cybershuttle Kernel',
-    execute: () => {
-      return fetch('http://74.235.88.134/kernelspecs?user=yasith', {
-        mode: 'cors'
-      })
-        .then(res => res.json())
-        .then(json => {
-          const clusters = Object.keys(json);
-          const cfg = json.gkeyll.metadata.kernel_provisioner.config;
-          const spec = cfg.spec;
-          const workdir = String(cfg.workdir);
-          // create widget with default values returned from API
-          const content = new CybershuttleKernelLauncher(
-            clusters,
-            workdir,
-            spec
-          );
-          // Show widget as dialog and get response
-          return showDialog<any>({
-            title: 'Pick Cybershuttle Kernel',
-            body: content,
-            buttons: [
-              Dialog.cancelButton(),
-              Dialog.okButton({ label: 'Launch' })
-            ]
-          });
-        })
-        .then(result => {
-          // handle dialog output
-          if (result.button.accept) {
-            app.commands.execute(CMD_LAUNCH, result.value);
+    label: 'Select Cybershuttle Kernel',
+    execute: async (args: any) => {
+      try {
+        const res = await fetch(
+          'http://74.235.88.134/kernelspecs?user=yasith',
+          {
+            mode: 'cors'
           }
-        })
-        .catch(err => {
-          console.error(err);
+        );
+        const json = await res.json();
+        const clusters = Object.keys(json);
+        const cfg = json.gkeyll.metadata.kernel_provisioner.config;
+        const spec = cfg.spec;
+        const workdir = String(cfg.workdir);
+        // create dialog to provide kernel specs
+        const dialog = new Dialog<any>({
+          title: 'Select Kernel',
+          body: new CybershuttleKernelLauncher(clusters, workdir, spec),
+          buttons: [Dialog.cancelButton(), Dialog.okButton({ label: 'Launch' })]
         });
+        // handle dialog output
+        const result = await dialog.launch();
+        if (result.button.accept) {
+          console.log(args.action, result.value);
+          await app.commands.execute(args.action, result.value);
+        }
+      } catch (err) {
+        console.error(err);
+      }
     }
   });
 
   launcher.add({
     command: CMD_CHOOSE,
     category: 'Cybershuttle',
+    args: { action: CMD_CREATE }
   });
+
+  app.docRegistry.addWidgetExtension('Notebook', new CybershuttleButton());
 }
 
 export default plugin;
--- cybershuttle_nbplugin/src/kernel_launcher.tsx
+++ cybershuttle_nbplugin/src/kernel_launcher.tsx
@@ -5,7 +5,7 @@ import React, { useState } from 'react';
 interface KernelSpec {
   clusters: string[];
   workdir: string;
-  defaults: {[key: string]: string};
+  config: { [key: string]: string };
   onChange: (value: any) => void;
 }
 
@@ -15,29 +15,43 @@ interface KernelSpec {
  * @returns The React component
  */
 const KernelSpecForm = (props: KernelSpec) => {
-  const [counter, setCounter] = useState(0);
+  const [cluster, setCluster] = useState(props.clusters[0]);
+  const [workdir, setWorkdir] = useState(props.workdir);
+  const [config, setConfig] = useState(props.config);
+
+  const onChange = (key: string, value: any) => {
+    switch (key) {
+      case 'cluster':
+        setCluster(_ => value);
+        break;
+      case 'workdir':
+        setWorkdir(_ => value);
+        break;
+      case 'config':
+        setConfig(prev => ({ ...prev, ...value }));
+        break;
+    }
+    props.onChange({ cluster, workdir, config })
+  };
   return (
     <div>
-      <p>You clicked {counter} times!</p>
       <div>
-        <select name="cluster">
-          {props.clusters.map(c => (
-            <option value={c}>{c}</option>
-          ))}
+        <label><b>Cluster</b></label>
+        <select name="cluster" value={cluster} onChange={e => onChange('cluster', e.target.value)}>
+          {props.clusters.map(c => (<option value={c}>{c}</option>))}
         </select>
-        <input name="workdir" type="text" value={props.workdir} title="Working Directory"></input>
-        {Object.entries(props.defaults).map(([k, v]) => (
-          <input name={k} type="text" value={v} title={k}></input>
-        ))}
       </div>
-      <button
-        onClick={(): void => {
-          setCounter(counter + 1);
-          // props.onChange(counter + 1);
-        }}
-      >
-        Increment
-      </button>
+      <div>
+        <label><b>Working Directory</b></label>
+        <input name="workdir" type="text" value={workdir} onChange={e => onChange('workdir', e.target.value)}></input>
+      </div>
+      <h3 style={{ fontWeight: 'normal' }}>Choose Resources</h3>
+      {Object.entries(config).map(([k, v]) => (
+        <div>
+          <label><b>{k}</b></label>
+          <input name={k} type="text" value={v} onChange={e => onChange('config', { [k]: e.target.value })}></input>
+        </div>
+      ))}
     </div>
   );
 };
@@ -48,31 +62,26 @@ const KernelSpecForm = (props: KernelSpec) => {
 class CybershuttleKernelLauncher extends ReactWidget {
   clusters: string[];
   workdir: string;
-  defaults: any;
-  spec: any;
+  config: any;
+  choice: any;
   /**
    * Constructs a new CounterWidget.
    */
-  constructor(clusters: string[], workdir: string, defaults: any) {
+  constructor(clusters: string[], workdir: string, config: any) {
     super();
     this.clusters = clusters;
     this.workdir = workdir;
-    this.defaults = defaults;
-    this.spec = { ...defaults }; // copy of defaults
+    this.config = config;
+    this.choice = { cluster: this.clusters[0], workdir: this.workdir, config: config };
   }
 
   getValue() {
-    return this.spec;
+    return this.choice;
   }
 
   render(): JSX.Element {
     return (
-      <KernelSpecForm
-        clusters={this.clusters}
-        workdir={this.workdir}
-        defaults={this.defaults}
-        onChange={value => {}}
-      />
+      <KernelSpecForm clusters={this.clusters} workdir={this.workdir} config={this.config} onChange={value => { this.choice = value; }} />
     );
   }
 }
